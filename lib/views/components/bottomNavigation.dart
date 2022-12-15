8124203fd6ad1b78ab5c2ab92fb6aff20df4d41e
ssabrut <michael.gunawan2002@gmail.com>
2022-12-15 23:06:36 +0700

    Add bottom navigation bar

---
+++ lib/views/components/bottomNavigation.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:supervision/views/orderPage.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 12, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOption = <Widget>[
+    OrderPage(),
+    Text(
+      "Index 1: History",
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _widgetOption.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.receipt), label: "Transaksi"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "Riwayat")
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
